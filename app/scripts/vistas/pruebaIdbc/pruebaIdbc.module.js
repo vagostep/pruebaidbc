46500e8f1415a43fbd7be9f18aef4315b1223016
vago <andres29diaz@gmail.com>
2017-03-04 03:59:55 -0400

    Creado modulo principal de la aplicacion

---
+++ app/scripts/vistas/pruebaIdbc/pruebaIdbc.module.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    /**
+    *
+    * @ngdoc interface
+    * @name pruebaIdbc
+    * @description
+    *
+    * Módulo principal de la aplicación pruebaIdbc.
+    **/
+    angular
+        .module('pruebaIdbc', ['navbar', 'informacionUsuario']);
+
+})();
